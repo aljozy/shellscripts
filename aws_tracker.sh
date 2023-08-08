a2ac4a08deb631af0487d0afb3731b6afdb50d58
aljozy <aljozy2@yahoo.com>
2023-08-08 16:50:05 +0530

    revamp script and enable debugging

--- aws_tracker.sh
+++ aws_tracker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #version 1
+#created by Aljozy Bazer
 #This script will report the AWS resource usage
 ########################
+set -x
 
 #AWS s3
 #AWS EC2
@@ -11,6 +13,30 @@
 
 
 #list s3 buckets
+echo "print list of s3 buckets"
 
 aws s3 ls
+
+#list EC2 Instances
+echo "print list of EC2"
+
+aws ec2 describe-instances
+
+#list lambda
+echo "print list of lambda functions"
+
+aws lambda list-functions
+
+#list IAM users
+echo "print list of IAM users"
+
+aws iam list-users
+
+
+
+
+
+
+
+
 
