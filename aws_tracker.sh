fcb708d800e8b925a4ff81e656a343bb0f6d7f0d
aljozy <aljozy2@yahoo.com>
2023-08-08 16:29:34 +0530

    aws cli shell script

---
+++ aws_tracker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#version 1
+#This script will report the AWS resource usage
+########################
+
+#AWS s3
+#AWS EC2
+#AWS Lambda
+#AWS IAM Users
+
+
+#list s3 buckets
+
+aws s3 ls
+
